80d6257cda8408b1e64809444e3a9d52b9bf13f4
axksel <jonas.aksel@gmail.com>
2015-11-04 13:25:01 +0100

    made the grid more nice
    nicer grid

--- src/Grid.java
+++ src/Grid.java
@@ -24,13 +24,13 @@ public class Grid {
 		 for(int r =0; r <3; r++){
 		    hexarray[0][r]=new Hexagon((r*50+_i)*2,(_k)*2);}
 		 for(int r =0; r <4; r++){
-			 hexarray[1][r]=new Hexagon ((r*50+_i-25)*2,(_k+50)*2);  }
+			 hexarray[1][r]=new Hexagon ((r*50+_i-25)*2,(_k+43)*2);  }
 		 for(int r =0; r <5; r++){
-			 hexarray[2][r]=new Hexagon ((r*50+_i-50)*2,(_k+100)*2);  }
+			 hexarray[2][r]=new Hexagon ((r*50+_i-50)*2,(_k+86)*2);  }
 		 for(int r =0; r <4; r++){
-			 hexarray[3][r]=new Hexagon ((r*50+_i-25)*2,(_k+150)*2);  }
+			 hexarray[3][r]=new Hexagon ((r*50+_i-25)*2,(_k+129)*2);  }
 		 for(int r =0; r <3; r++){
-			 hexarray[4][r]=new Hexagon ((r*50+_i)*2,(_k+200)*2);  }
+			 hexarray[4][r]=new Hexagon ((r*50+_i)*2,(_k+172)*2);  }
 				 
 		}	 
 				 
